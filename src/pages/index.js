f1878423c84c8eef2206fa26ca71c9705c0dcc9e
zach-wearebattalion <zach@wearebattalion.com>
2021-03-20 22:32:17 +0100

    feat(IntroSection): add layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,13 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import IntroSection from "../components/IntroSection"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <IntroSection />
+    <h1>Test</h1>
   </Layout>
 )
 
